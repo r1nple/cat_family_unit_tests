a187476250d2b756bf59ee8cd4ac246a85e0e214
rinat <rinatalbertovich24@gmail.com>
2026-04-22 16:12:23 +0300

    Создал конструктор с параметром feline

--- src/main/java/com/example/Lion.java
+++ src/main/java/com/example/Lion.java
@@ -7,7 +7,8 @@ public class Lion {
     boolean hasMane;
     private Feline feline;
 
-    public Lion(String sex) throws Exception {
+    public Lion(String sex, Feline feline) throws Exception {
+        this.feline = feline;
         if ("Самец".equals(sex)) {
             hasMane = true;
         } else if ("Самка".equals(sex)) {
